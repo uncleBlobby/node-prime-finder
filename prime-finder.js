0137db00591a0c5ff4892de6084105cdc5e3e4d1
Dustin Christianson <dc.christianson@gmail.com>
2022-05-01 10:57:45 -0600

    added timers to show time taken per thousand primes and average number of primes found per second

--- prime-finder.js
+++ prime-finder.js
@@ -1,10 +1,14 @@
 fs = require('fs');
+const START_TIME = new Date().getTime();
 
 console.log(`Prime Finder`)
-
+console.log(`Start Time: ${START_TIME}`)
 console.log(`Go!`)
 
 let i = 1;
+let totalPrimesFound = 0;
+
+
 
 function WritePrimeToFile(prime) {
     fs.appendFileSync('primes.txt', appendNewline(prime), 'utf-8', function(err) {
@@ -27,6 +31,17 @@ while(true) {
     }
     if (isPrime == true) {
         WritePrimeToFile(candidate)
+        totalPrimesFound++;
+        printTotalPrimesNowAndThen(totalPrimesFound)
     }
     i++;
+}
+
+function printTotalPrimesNowAndThen(total){
+    let timeNow = new Date().getTime();
+    let elapsedTime = timeNow - START_TIME;
+    if (total % 1000 == 0){
+        console.log(`Found ${total} primes in ${elapsedTime} milliseconds.`)
+        console.log(`Average: ${(total/elapsedTime) * 1000} primes per second.`)
+    }
 }
